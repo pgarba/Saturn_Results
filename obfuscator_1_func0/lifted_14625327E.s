d449a96d3f672ab1884f96e04f54d059d2cec58c
pgarba <pgarba@users.noreply.github.com>
2019-08-23 10:21:09 +0200

    Compare to original code

---
+++ obfuscator_1_func0/lifted_14625327E.s
@@ -0,0 +1,19 @@
+	.text
+	.intel_syntax noprefix
+	.file	"lifted_code"
+	.globl	F_14625327e_args        # -- Begin function F_14625327e_args
+	.p2align	4, 0x90
+	.type	F_14625327e_args,@function
+F_14625327e_args:                       # @F_14625327e_args
+	.cfi_startproc
+# %bb.0:                                # %entry
+	movabs	rax, 5401364432
+	mov	rax, qword ptr [rax]
+	ret
+.Lfunc_end0:
+	.size	F_14625327e_args, .Lfunc_end0-F_14625327e_args
+	.cfi_endproc
+                                        # -- End function
+
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
---
+++ obfuscator_1_func1/lifted_146253057.s
@@ -0,0 +1,19 @@
+	.text
+	.intel_syntax noprefix
+	.file	"lifted_code"
+	.globl	F_146253057_args        # -- Begin function F_146253057_args
+	.p2align	4, 0x90
+	.type	F_146253057_args,@function
+F_146253057_args:                       # @F_146253057_args
+	.cfi_startproc
+# %bb.0:                                # %entry
+	movabs	rax, 5401364440
+	mov	rax, qword ptr [rax]
+	ret
+.Lfunc_end0:
+	.size	F_146253057_args, .Lfunc_end0-F_146253057_args
+	.cfi_endproc
+                                        # -- End function
+
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
